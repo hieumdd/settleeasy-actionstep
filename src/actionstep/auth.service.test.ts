0284ccd46139879b38d84dbf1a50806764cfcc6d
HM <hieumdd@gmail.com>
2023-10-06 23:29:41 +0700

    feat: Add getToken

--- src/actionstep/auth.service.test.ts
+++ src/actionstep/auth.service.test.ts
@@ -1,4 +1,4 @@
-import { getAccessToken, getAuthorizationURL } from './auth.service';
+import { exchangeCodeForToken, getAuthorizationURL, getToken } from './auth.service';
 
 it('getAuthorizationURL', () => {
     const url = getAuthorizationURL();
@@ -6,10 +6,22 @@ it('getAuthorizationURL', () => {
     expect(url).toBeDefined();
 });
 
-it('getAccessToken', async () => {
+it('exchangeCodeForToken', async () => {
     const code = '01511b0c75af937bf404b829459674fe8769b323';
-    return getAccessToken(code)
+    return exchangeCodeForToken(code)
         .then((token) => expect(token).toBeDefined())
+        .catch((error) => {
+            console.error(error);
+            throw error;
+        });
+});
+
+it('getToken', async () => {
+    return getToken()
+        .then((token) => {
+            console.log(token.token);
+            expect(token.expired()).toBe(false);
+        })
         .catch((error) => {
             console.error(error);
             throw error;
--- src/actionstep/auth.service.ts
+++ src/actionstep/auth.service.ts
@@ -1,3 +1,4 @@
+import { Timestamp } from '@google-cloud/firestore';
 import { AuthorizationCode } from 'simple-oauth2';
 
 import * as tokenRepository from './token.repository';
@@ -8,25 +9,39 @@ const client = new AuthorizationCode({
         secret: process.env.ACTIONSTEP_CLIENT_SECRET ?? '',
     },
     auth: {
-        authorizeHost: 'https://go.actionstepstaging.com',
-        authorizePath: '/api/oauth/authorize',
-        tokenHost: 'https://api.actionstepstaging.com',
-        tokenPath: '/api/oauth/token',
+        authorizeHost: 'https://go.actionstepstaging.com/',
+        authorizePath: 'api/oauth/authorize',
+        tokenHost: 'https://api.actionstepstaging.com/',
+        tokenPath: 'api/oauth/token',
     },
+    http: { json: 'force' },
 });
 
 export const getAuthorizationURL = () => {
     return client.authorizeURL({ redirect_uri: process.env.ACTIONSTEP_REDIRECT_URI, scope: 'all' });
 };
 
-export const getAccessToken = async (code: string) => {
-    const accessToken = await client.getToken({
+export const exchangeCodeForToken = async (code: string) => {
+    const { token } = await client.getToken({
         code,
         redirect_uri: process.env.ACTIONSTEP_REDIRECT_URI ?? '',
         scope: 'all',
     });
 
-    await tokenRepository.set(accessToken.token);
+    await tokenRepository.set(token);
+    return token;
+};
 
-    return accessToken.token;
+export const getToken = async () => {
+    const existingToken = await tokenRepository.get().then((token) => {
+        return client.createToken({ ...token, expires_at: (<Timestamp>token.expires_at).toDate() });
+    });
+
+    if (existingToken.expired(300_000)) {
+        const newToken = await existingToken.refresh();
+        await tokenRepository.set(newToken.token);
+        return newToken;
+    }
+
+    return existingToken;
 };
